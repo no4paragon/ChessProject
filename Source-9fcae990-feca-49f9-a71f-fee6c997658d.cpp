fe974bd85a2b50da2018099d493a5c0af6eeb794
no4paragon <46017825+no4paragon@users.noreply.github.com>
2023-04-04 19:46:30 -0700

    Add files via upload

---
+++ Source-9fcae990-feca-49f9-a71f-fee6c997658d.cpp
@@ -0,0 +1,17 @@
+#include "Board.h"
+#include "Piece.h"
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    Board b;
+    b.selectPiece();
+
+
+
+
+    return 0;
+}
